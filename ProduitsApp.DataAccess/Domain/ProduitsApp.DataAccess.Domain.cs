628707901f376c6ff7be8adf8d7ccf89f9e5581a
HichemGRIRA <grirah@gmail.com>
2018-04-27 15:49:05 +0100

    first commit

---
+++ ProduitsApp.DataAccess/Domain/ProduitsApp.DataAccess.Domain.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProduitsApp.DataAccess.Repository
+{
+    class Produit
+    {
+        public string Id { get; set; }
+        public string libelle { get; set; }
+        public decimal PrixUnitaire { get; set; }
+    }
+}
